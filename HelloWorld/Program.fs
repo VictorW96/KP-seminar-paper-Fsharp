2ec528f5f8bdd6df104bf9a1da86328d1a42ed08
Victor <victorwolf@outlook.de>
2019-11-07 16:45:20 +0100

    structural changes

---
+++ HelloWorld/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open BasicFunctions
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let result1 :int = sampleFunction1 2000
+    printfn "The result of squaring the integer 2000 and adding 3 is %d" result1
+    0
+    
