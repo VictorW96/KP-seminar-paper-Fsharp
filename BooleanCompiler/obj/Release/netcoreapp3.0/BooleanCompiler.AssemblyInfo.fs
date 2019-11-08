cdfeb522dd9ff85461b7aaa376820f7fe7c4a781
Victor <victorwolf@outlook.de>
2019-11-08 11:28:58 +0100

    structural changes 2

--- BooleanCompiler/obj/Release/netcoreapp3.0/BooleanCompiler.AssemblyInfo.fs
+++ BooleanCompiler/obj/Release/netcoreapp3.0/BooleanCompiler.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("KP-seminar-paper-Fsharp")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("BooleanCompiler")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Release")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("KP-seminar-paper-Fsharp")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("KP-seminar-paper-Fsharp")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("BooleanCompiler")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("BooleanCompiler")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
