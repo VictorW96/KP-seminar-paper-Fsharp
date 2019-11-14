430dbb3b8d61014ae2deb79e17f5baa7bf023026
Victor Wolf <sWMAviwolf@stud.fh-rosenheim.de>
2019-11-14 16:48:44 +0100

    basic functions

--- HelloWorld/Basicfunctions.fs
+++ HelloWorld/Basicfunctions.fs
@@ -1,4 +1,13 @@
 ﻿module BasicFunctions
 
+let prefix prefixStr baseStr =
+    prefixStr + "," + baseStr
 
+let exclaim s =
+    s + "!"
 
+let names = ["Victor"; "Petra"; "Test"]
+
+let result = names
+             |> List.map (prefix "Hello")
+             |> List.map exclaim
