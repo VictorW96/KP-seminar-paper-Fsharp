c74793f6e734cbf6c5f5f49dd9f33dffc9694146
Victor Wolf <sWMAviwolf@stud.fh-rosenheim.de>
2019-11-16 13:31:21 +0100

    first parser combinator fucntions

--- BooleanCompiler/Parser.fs
+++ BooleanCompiler/Parser.fs
@@ -22,7 +22,31 @@ type Result<'a> =
     | Success of 'a
     | Failure of string 
 
+
 type Parser<'T> = Parser of (string -> Result<'T * string>)
+
+let pchar charToMatch = 
+    // define a nested inner function
+    let innerFn str =
+        if String.IsNullOrEmpty(str) then
+            Failure "No more input"
+        else
+            let first = str.[0] 
+            if first = charToMatch then
+                let remaining = str.[1..]
+                Success (charToMatch,remaining)
+            else
+                let msg = sprintf "Expecting '%c'. Got '%c'" charToMatch first
+                Failure msg
+    // return the inner function
+    Parser innerFn 
+
+let run parser input = 
+    // unwrap parser to get inner function
+    let (Parser innerFn) = parser 
+    // call inner function with input
+    innerFn input
+
 
 let andThen parser1 parser2 =
     let innerFn input =
@@ -53,4 +77,58 @@ let andThen parser1 parser2 =
 
     // return the inner function
     Parser innerFn 
-    
+
+let ( .>>. ) = andThen 
+
+let orElse parser1 parser2 =
+    let innerFn input =
+        // run parser1 with the input
+        let result1 = run parser1 input
+
+        // test the result for Failure/Success
+        match result1 with
+        | Success result -> 
+            // if success, return the original result
+            result1
+
+        | Failure err -> 
+            // if failed, run parser2 with the input
+            let result2 = run parser2 input
+
+            // return parser2's result
+            result2 
+
+    // return the inner function
+    Parser innerFn 
+
+let ( <|> ) = orElse
+
+/// Choose any of a list of parsers
+let choice listOfParsers = 
+    List.reduce ( <|> ) listOfParsers 
+
+let anyOf listOfChars = 
+    listOfChars
+    |> List.map pchar // convert into parsers
+    |> choice
+
+let mapP f parser =
+    let innerFn input =
+        // run parser with the input
+        let result = run parser input
+
+        // test the result for Failure/Success
+        match result with
+        | Success (value,remaining) ->
+            // if success, return the value transformed by f
+            let newValue = f value
+            Success (newValue, remaining)
+
+        | Failure err ->
+            // if failed, return the error
+            Failure err
+    // return the inner function
+    Parser innerFn
+
+let ( <!> ) = mapP
+
