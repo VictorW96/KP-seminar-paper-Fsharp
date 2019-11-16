c6dccd74cd3dde3b182855a21937397f242b8d60
Victor Wolf <sWMAviwolf@stud.fh-rosenheim.de>
2019-11-16 14:33:34 +0100

    Fparsec first attempts

--- BooleanCompiler/BooleanParser.fs
+++ BooleanCompiler/BooleanParser.fs
@@ -1,2 +1,18 @@
 ﻿module BooleanParser
 
+open FParsec
+
+//  ---------------------------------------------------------
+// The expression should have the following EBNF form:
+//  EBNF
+//  ---------------------------------------------------------
+// 	<expression> ::= <term> { <or> <term> }
+// 	<term> ::= <factor> { <and> <factor> }
+// 	<factor> ::= <var> | <not> <factor> | (<expression>)
+// 	<or>  ::= '|'
+// 	<and> ::= '&'
+// 	<not> ::= '!'
+//  <var> ::= '[a-zA-Z0-9]*'
+//  ---------------------------------------------------------
+
+let result = run pfloat "02"
