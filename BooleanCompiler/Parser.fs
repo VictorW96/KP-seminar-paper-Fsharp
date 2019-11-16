2242a4e0f5e9985d418bbbc42affab90998f4a77
Victor Wolf <sWMAviwolf@stud.fh-rosenheim.de>
2019-11-16 14:16:16 +0100

    Parser combinator library

--- BooleanCompiler/Parser.fs
+++ BooleanCompiler/Parser.fs
@@ -1,30 +1,24 @@
 ﻿module Parser
+(*
+ParserLibrary_v2.fsx
 
-open AST
-open FParsec
+Version 2 of the code for a parser library.
+
+Related blog post: http://fsharpforfunandprofit.com/posts/understanding-parser-combinators-2/
+
+*)
+
 open System
 
-//  ---------------------------------------------------------
-// The expression should have the following EBNF form:
-//  EBNF
-//  ---------------------------------------------------------
-// 	<expression> ::= <term> { <or> <term> }
-// 	<term> ::= <factor> { <and> <factor> }
-// 	<factor> ::= <var> | <not> <factor> | (<expression>)
-// 	<or>  ::= '|'
-// 	<and> ::= '&'
-// 	<not> ::= '!'
-//  <var> ::= '[a-zA-Z0-9]*'
-//  ---------------------------------------------------------
-
-
+/// Type that represents Success/Failure in parsing
 type Result<'a> =
     | Success of 'a
     | Failure of string 
 
-
+/// Type that wraps a parsing function
 type Parser<'T> = Parser of (string -> Result<'T * string>)
 
+/// Parse a single character
 let pchar charToMatch = 
     // define a nested inner function
     let innerFn str =
@@ -38,52 +32,80 @@ let pchar charToMatch =
             else
                 let msg = sprintf "Expecting '%c'. Got '%c'" charToMatch first
                 Failure msg
-    // return the inner function
+    // return the "wrapped" inner function
     Parser innerFn 
 
+/// Run a parser with some input
 let run parser input = 
     // unwrap parser to get inner function
     let (Parser innerFn) = parser 
     // call inner function with input
     innerFn input
 
-
-let andThen parser1 parser2 =
+/// "bindP" takes a parser-producing function f, and a parser p
+/// and passes the output of p into f, to create a new parser
+let bindP f p =
     let innerFn input =
-        // run parser1 with the input
-        let result1 = run parser1 input
-        
-        // test the result for Failure/Success
+        let result1 = run p input 
         match result1 with
         | Failure err -> 
             // return error from parser1
             Failure err  
+        | Success (value1,remainingInput) ->
+            // apply f to get a new parser
+            let p2 = f value1
+            // run parser with remaining input
+            run p2 remainingInput
+    Parser innerFn 
 
-        | Success (value1,remaining1) -> 
-            // run parser2 with the remaining input
-            let result2 =  run parser2 remaining1
-            
-            // test the result for Failure/Success
-            match result2 with 
-            | Failure err ->
-                // return error from parser2
-                Failure err 
-            
-            | Success (value2,remaining2) -> 
-                // combine both values as a pair
-                let newValue = (value1,value2)
-                // return remaining input after parser2
-                Success (newValue,remaining2)
+/// Infix version of bindP
+let ( >>= ) p f = bindP f p
 
+/// Lift a value to a Parser
+let returnP x = 
+    let innerFn input =
+        // ignore the input and return x
+        Success (x,input)
     // return the inner function
     Parser innerFn 
 
-let ( .>>. ) = andThen 
+/// apply a function to the value inside a parser
+let mapP f = 
+    bindP (f >> returnP)
 
-let orElse parser1 parser2 =
+/// infix version of mapP
+let ( <!> ) = mapP
+
+/// "piping" version of mapP
+let ( |>> ) x f = mapP f x
+
+/// apply a wrapped function to a wrapped value
+let applyP fP xP =         
+    fP >>= (fun f -> 
+    xP >>= (fun x -> 
+        returnP (f x) ))
+
+/// infix version of apply
+let ( <*> ) = applyP
+
+/// lift a two parameter function to Parser World
+let lift2 f xP yP =
+    returnP f <*> xP <*> yP
+
+/// Combine two parsers as "A andThen B"
+let andThen p1 p2 =         
+    p1 >>= (fun p1Result -> 
+    p2 >>= (fun p2Result -> 
+        returnP (p1Result,p2Result) ))
+
+/// Infix version of andThen
+let ( .>>. ) = andThen
+
+/// Combine two parsers as "A orElse B"
+let orElse p1 p2 =
     let innerFn input =
         // run parser1 with the input
-        let result1 = run parser1 input
+        let result1 = run p1 input
 
         // test the result for Failure/Success
         match result1 with
@@ -93,7 +115,7 @@ let orElse parser1 parser2 =
 
         | Failure err -> 
             // if failed, run parser2 with the input
-            let result2 = run parser2 input
+            let result2 = run p2 input
 
             // return parser2's result
             result2 
@@ -101,67 +123,20 @@ let orElse parser1 parser2 =
     // return the inner function
     Parser innerFn 
 
+/// Infix version of orElse
 let ( <|> ) = orElse
 
 /// Choose any of a list of parsers
 let choice listOfParsers = 
     List.reduce ( <|> ) listOfParsers 
 
+/// Choose any of a list of characters
 let anyOf listOfChars = 
     listOfChars
     |> List.map pchar // convert into parsers
     |> choice
 
-let mapP f parser =
-    let innerFn input =
-        // run parser with the input
-        let result = run parser input
-
-        // test the result for Failure/Success
-        match result with
-        | Success (value,remaining) ->
-            // if success, return the value transformed by f
-            let newValue = f value
-            Success (newValue, remaining)
-
-        | Failure err ->
-            // if failed, return the error
-            Failure err
-    // return the inner function
-    Parser innerFn
-
-let ( <!> ) = mapP
-
-let ( |>> ) x f = mapP f x
-
-let returnP x =
-    let innerFn input =
-        // ignore the input and return x
-        Success (x,input )
-    // return the inner function
-    Parser innerFn
-
-let applyP fP xP =
-    // create a Parser containing a pair (f,x)
-    (fP .>>. xP)
-    // map the pair by applying f to x
-    |> mapP (fun (f,x) -> f x)
-
-let ( <*> ) = applyP
-
-// lift a two parameter function to Parser World
-let lift2 f xP yP =
-    returnP f <*> xP <*> yP
-
-let addP =
-    lift2 (+)
-
-let startsWith (str:string) (prefix:string) =
-    str.StartsWith(prefix)
-
-let startsWithP =
-    lift2 startsWith
-
+/// Convert a list of Parsers into a Parser of a list
 let rec sequence parserList =
     // define the "cons" function, which is a two parameter function
     let cons head tail = head::tail
@@ -171,52 +146,74 @@ let rec sequence parserList =
 
     // process the list of parsers recursively
     match parserList with
-    | [] ->
+    | [] -> 
         returnP []
     | head::tail ->
         consP head (sequence tail)
 
-let parsers = [ pchar 'A'; pchar 'B'; pchar 'C' ]
-let combined = sequence parsers
-        
-let result = run combined "ABCD"
-
-/// Helper to create a string from a list of chars
-let charListToStr charList =
-     String(List.toArray charList)
-
-// match a specific string
-let pstring str =
-    str
-    // convert to list of char
-    |> List.ofSeq
-    // map each char to a pchar
-    |> List.map pchar
-    // convert to Parser<char list>
-    |> sequence
-    // convert Parser<char list> to Parser<string>
-    |> mapP charListToStr
-
+/// (helper) match zero or more occurences of the specified parser
 let rec parseZeroOrMore parser input =
     // run parser with the input
-    let firstResult = run parser input
+    let firstResult = run parser input 
     // test the result for Failure/Success
     match firstResult with
-    | Failure err ->
+    | Failure err -> 
         // if parse fails, return empty list
-        ([],input)
-    | Success (firstValue,inputAfterFirstParse) ->
+        ([],input)  
+    | Success (firstValue,inputAfterFirstParse) -> 
         // if parse succeeds, call recursively
         // to get the subsequent values
-        let (subsequentValues,remainingInput) =
+        let (subsequentValues,remainingInput) = 
             parseZeroOrMore parser inputAfterFirstParse
         let values = firstValue::subsequentValues
-        (values,remainingInput)
+        (values,remainingInput)  
 
-let many parser =
+/// matches zero or more occurences of the specified parser
+let many parser = 
 
     let rec innerFn input =
         // parse the input -- wrap in Success as it always succeeds
         Success (parseZeroOrMore parser input)
 
     Parser innerFn
+
+/// matches one or more occurences of the specified parser
+let many1 p =         
+    p      >>= (fun head -> 
+    many p >>= (fun tail -> 
+        returnP (head::tail) ))
+
+/// Parses an optional occurrence of p and returns an option value.
+let opt p = 
+    let some = p |>> Some
+    let none = returnP None
+    some <|> none
+
+/// Keep only the result of the left side parser
+let (.>>) p1 p2 = 
+    // create a pair
+    p1 .>>. p2 
+    // then only keep the first value
+    |> mapP (fun (a,b) -> a) 
+
+/// Keep only the result of the right side parser
+let (>>.) p1 p2 = 
+    // create a pair
+    p1 .>>. p2 
+    // then only keep the second value
+    |> mapP (fun (a,b) -> b) 
+
+/// Keep only the result of the middle parser
+let between p1 p2 p3 = 
+    p1 >>. p2 .>> p3 
+
+/// Parses one or more occurrences of p separated by sep
+let sepBy1 p sep =
+    let sepThenP = sep >>. p            
+    p .>>. many sepThenP 
+    |>> fun (p,pList) -> p::pList
+
+/// Parses zero or more occurrences of p separated by sep
+let sepBy p sep =
+    sepBy1 p sep <|> returnP []
+
