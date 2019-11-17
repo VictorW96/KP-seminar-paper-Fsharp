1a4993d9562f9ae2633405bc6c7a6866a6f78c9f
Victor Wolf <sWMAviwolf@stud.fh-rosenheim.de>
2019-11-17 16:26:45 +0100

    command line argument integration

--- BooleanCompiler/Program.fs
+++ BooleanCompiler/Program.fs
@@ -5,7 +5,24 @@ open BooleanParser
 open FParsec
 open AST
 
+let splitKeyVal (str : string) =
+    match str.Split(':') with
+    |[|key; value|] ->  (key, System.Boolean.Parse(value))
+    |_ -> invalidArg "str" "str must have the format key:value"
+
+let createMap (str : string) =
+    str.Split('|') 
+    |> Array.map (splitKeyVal)
+    |> Map
+
 [<EntryPoint>]
-let main argv =
-    test parseExpression "!!A&(!B|C)"
+let main (args:string[]) =
+    if args.Length <> 2 then
+        failwith "Error: Expected arguments <varBoolMap> and <stringToParse>"
+    let stringvarBoolMap, stringToParse = args.[0], args.[1]
+    let varBoolMap = createMap stringvarBoolMap
+
+    match run parseExpression stringToParse with
+        | Success(result, _, _)   -> printfn "Success: %A evaluated as %b" result (eval varBoolMap result)
+        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
     0
